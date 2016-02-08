2cc3e3d0ef8317b885e6d9a94f42f5aa2bdb6a41
Monika Kowalczyk <m.kowalczyk@kownet.info>
2016-02-08 10:38:52 +0100

    correction

--- app/components/kontakt/kontakt.controller.js
+++ app/components/kontakt/kontakt.controller.js
@@ -4,8 +4,24 @@
     angular.module('portfolio')
         .controller('kontaktController', kontaktController);
 
-    function kontaktController(){
+     function kontaktController(){
+         var vm = this;
 
-    }
+     	vm.title = 'Contact us';
+
+     	vm.message = {
+     		name: 'monikakowalczyk',
+     		email: 'm.kowalczyk@kownet.info',
+     		content: 'Some content here'
+     	}
+
+     	vm.submitMessage = submitMessage;
+
+	     function submitMessage(message){
+	     	console.log('message', message)
+	     }
+
+
+     }
     
 })();
